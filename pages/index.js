189f1517f8490ce5c0fa625f3a09a9592cfd14c7
Alegiannx <alegiannx@gmail.com>
2020-10-08 23:06:24 +0300

    added landing page with basic CSS

--- pages/index.js
+++ pages/index.js
@@ -3,58 +3,48 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.body}>
       <Head>
-        <title>Create Next App</title>
+        <title>ShoppingAddiction</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossOrigin="anonymous" />
       </Head>
-
+      <nav className={styles.navbar}>
+        <a className={styles.navbar_link}><i className="fas fa-home"></i></a>
+        <a className={styles.navbar_link}>Collections</a>
+        <a className={styles.navbar_link}>Brands</a>
+        <a
+          className={`${styles.navbar_link} ${styles.navbar_last}`}
+        >
+          Login<i className="fas fa-door-open"></i>
+        </a>
+      </nav>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Alex is learning Next.js!
-        </h1>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.jumbotron + " " + styles.blue}>
+          <div className={styles.container}>
+            <h1 className={styles.title}>
+              Welcome to Shopping Addiction!
+            </h1>
+            <h2 className={styles.title_desc}>... you literally won't stop buying</h2>
+            <div className={`${styles.hcenter} ${styles.mt}`}>
+              <button className={styles.mainButton}>Find Out More</button>
+            </div>
+          </div>
         </div>
+        <p>lorem</p>
+        <p>lorem</p>
+        <p>lorem</p>
+        <p>lorem</p>
+        <p>lorem</p>
+        <p>lorem</p>
+        <p>lorem</p>
+        <p>lorem</p>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>made by Alex @ knowledge</p>
       </footer>
-    </div>
+    </div >
   )
 }
